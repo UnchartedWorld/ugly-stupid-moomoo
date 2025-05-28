87dad8a880e759cde00d9fa68b207b6ed49c1df6
UnchartedWorld <nathangvjcollins@gmail.com>
2025-05-28 23:58:28 +0100

    feat: Implements basic PartyManager singleton At current, all this does is parse a pre-existing Resource file & adds it to the party. Long term, this should allow me to add party members as I see fit.

--- Scripts/Main/PartyManager.cs
+++ Scripts/Main/PartyManager.cs
@@ -1,15 +1,47 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class PartyManager : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	private const string PlayerCharacterStatPath = "res://Scripts/Resources/Characters/Linh.tres";
+	public static PartyManager Instance { get; private set; }
+
+	public IReadOnlyCollection<PartyMember> PartyMembers => CurrentParty;
+	public List<PartyMember> CurrentParty { get; private set; } = [];
+
 	public override void _Ready()
 	{
+		CharacterResource loadedStats = ResourceLoader.Load<CharacterResource>(PlayerCharacterStatPath);
+		if (loadedStats != null)
+		{
+			CharacterStats mainCharacterStats = new(
+				loadedStats.CharacterName,
+				loadedStats.MaxHealth,
+				loadedStats.MaxMoomooPoints,
+				loadedStats.Attack,
+				loadedStats.Defense,
+				loadedStats.Agility);
+
+			AddPartyMember(mainCharacterStats);
+		}
+
+		Instance = this;
+    }
+
+	public void AddPartyMember(CharacterStats stats)
+	{
+		CurrentParty.Add(new PartyMember(stats));
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void RemovePartyMember(PartyMember member)
 	{
+		CurrentParty.Remove(member);
+	}
+
+	public PartyMember GetPartyMember(int index)
+	{
+		return CurrentParty.ElementAtOrDefault(index);
 	}
 }
