7094db20ec04a87f1f6a72b371f8abc41723b75a
UnchartedWorld <nathangvjcollins@gmail.com>
2025-06-10 23:15:06 +0100

    feat: Adds ability to add characters to party This also adds that functionality to occur from dialogues. Neat.

--- Scripts/Main/PartyManager.cs
+++ Scripts/Main/PartyManager.cs
@@ -16,23 +16,78 @@ public partial class PartyManager : Node
 		CharacterResource loadedStats = ResourceLoader.Load<CharacterResource>(PlayerCharacterStatPath);
 		if (loadedStats != null)
 		{
+			List<Skill> listOfSkills = [];
+			foreach (var skill in loadedStats.StartingSkills)
+			{
+				Skill translatedSkill = new
+				(
+					skill.SkillName,
+					skill.BaseDamage,
+					skill.Element,
+					skill.MMPCost,
+					skill.isHealing,
+					skill.healAmount,
+					skill.TargetType
+				);
+
+				listOfSkills.Add(translatedSkill);
+			}
 			CharacterStats mainCharacterStats = new(
 				loadedStats.CharacterName,
 				loadedStats.MaxHealth,
 				loadedStats.MaxMoomooPoints,
 				loadedStats.Attack,
 				loadedStats.Defense,
-				loadedStats.Agility);
+				loadedStats.Agility,
+				loadedStats.Resistances.ToDictionary(),
+				listOfSkills);
 
 			AddPartyMember(mainCharacterStats);
 		}
 
 		Instance = this;
-    }
+	}
 
 	public void AddPartyMember(CharacterStats stats)
 	{
 		CurrentParty.Add(new PartyMember(stats));
+	}
+
+	public void AddPartyMemberByDialogue(Variant arg)
+	{
+		CharacterResource character = arg.As<CharacterResource>();
+
+		List<Skill> listOfSkills = [];
+		foreach (var skill in character.StartingSkills)
+		{
+			Skill translatedSkill = new
+			(
+				skill.SkillName,
+				skill.BaseDamage,
+				skill.Element,
+				skill.MMPCost,
+				skill.isHealing,
+				skill.healAmount,
+				skill.TargetType
+			);
+
+			listOfSkills.Add(translatedSkill);
+
+			CharacterStats characterStats = new
+			(
+				character.CharacterName,
+				character.MaxHealth,
+				character.MaxMoomooPoints,
+				character.Attack,
+				character.Defense,
+				character.Agility,
+				character.Resistances.ToDictionary(),
+				listOfSkills
+			);
+
+			CurrentParty.Add(new PartyMember(characterStats));
+
+		}
 	}
 
 	public void RemovePartyMember(PartyMember member)
